e312f67026ec194b290f42aa69b16e050655533c
Tajmirul <tasmirolislam@gmail.com>
2020-10-15 06:40:53 +0600

    minified files

--- js/main.js
+++ js/main.js
@@ -1,28 +1,4 @@
 $(document).ready(function () {
-
-    /* magnificPopup img view */
-    // $('.popup-image').magnificPopup({
-    //     type: 'image',
-    //     gallery: {
-    //         enabled: true
-    //     }
-    // });
-
-    /* magnificPopup video view */
-    // $('.popup-video').magnificPopup({
-    //     type: 'iframe'
-    // });
-
-    //for menu active class
-    // $('.portfolio-menu button').on('click', function (event) {
-    //     $(this).siblings('.active').removeClass('active');
-    //     $(this).addClass('active');
-    //     event.preventDefault();
-    // });
-
-    // WOW active
-    // new WOW().init();
-
     // mixitup --------------------
     var filterContainer = $('.filter-container');
     if (filterContainer.length != 0){
@@ -44,32 +20,6 @@ $(document).ready(function () {
             }
         ]
     });
-
-    // my code ----------------------------------------------------------------------
-    // preloader
-    // $(".preloader").animate({
-    //     top: -2000000
-    // }, 1500);
-
-    // sticky menu ber and go to top button
-    // $(window).scroll(function () {
-    //     var scrolled = $(window).scrollTop();
-    //     var topMain = $('main').offset().top - 400;
-
-    //     // go to top button
-    //     if (scrolled > topMain) {
-    //         $('.go-to-top').addClass('active');
-    //         $('.nav-outer').addClass('sticky');
-
-    //         $('.go-to-top').click(function () {
-    //             $(window).scrollTop(0);
-    //         });
-    //     } else {
-    //         $('.go-to-top').removeClass('active');
-    //         $('.nav-outer').removeClass('sticky')
-    //     }
-    //     // console.log(scrolled);
-    // });
 
     // menu bar open -------------------------
     $('.navigation-open').click(function () {
